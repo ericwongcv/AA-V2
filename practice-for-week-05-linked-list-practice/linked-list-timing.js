8907dbd07cc88b041caf9fdab41ff1cf1f321033
Eric Wong <contact.eric.wong@gmail.com>
2022-05-24 22:39:57 -0700

    make small change to linked list practice

--- practice-for-week-05-linked-list-practice/linked-list-timing.js
+++ practice-for-week-05-linked-list-practice/linked-list-timing.js
@@ -10,32 +10,32 @@ Construct a timing test to verify the time complexities of `addToHead` and
 
 let linkedList = new LinkedList();
 let doublyLinkedList = new DoublyLinkedList();
+let n = 10000;
 
 
 let linkedHeadStartTime = Date.now();
-
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < n; i++) {
     linkedList.addToHead(i);
 }
 let linkedHeadEndTime = Date.now();
 console.log(`${linkedHeadEndTime - linkedHeadStartTime}ms`); // O(1) Time
 
 let linkedTailStartTime = Date.now();
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < n; i++) {
     linkedList.addToTail(i);
 }
 let linkedTailEndTime = Date.now();
 console.log(`${linkedTailEndTime - linkedTailStartTime}ms`); // O(n) Time
 
 let doubleHeadStartTime = Date.now();
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < n; i++) {
     doublyLinkedList.addToHead(i);
 }
 let doubleHeadEndTime = Date.now();
 console.log(`${doubleHeadEndTime - doubleHeadStartTime}ms`); // O(1) Time
 
 let doubleTailStartTime = Date.now();
-for (let i = 0; i < 10000; i++) {
+for (let i = 0; i < n; i++) {
     doublyLinkedList.addToTail(i);
 }
 let doubleTailEndTime = Date.now();
