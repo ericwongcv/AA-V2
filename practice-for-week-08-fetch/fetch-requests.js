d864c5024383b0dfbdd30f503329d816ad2a17e2
Eric Wong <contact.eric.wong@gmail.com>
2022-06-15 15:52:30 -0700

    submit fetch practice

---
+++ practice-for-week-08-fetch/fetch-requests.js
@@ -0,0 +1,58 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============== 1. Print the status code of the response =============== */
+
+// Your code here
+
+// 200 OK
+
+/* ====== 2. Print true if the status of the response was successful ====== */
+
+// Your code here
+
+// True
+
+/* =================== 3. Print the Content-Type Header =================== */
+
+// Your code here
+
+// text/html; charset=utf-8
+
+/* ============== 4. Print the body of the response as text =============== */
+
+// Your code here
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+
+// <head>
+// 	<meta charset="utf-8">
+// 	<title>Products List</title>
+// 	<link rel="stylesheet" href="/assets/css/application.css">
+// </head>
+
+// <body>
+// 	<nav>
+// 		<ul>
+// 			<li><a href="/products">See all Products </a></li>
+// 			<li><a href="/products/best-selling">Best-Selling Product</a></li>
+// 			<li><a href="/categories/grocery/products">Grocery</a></li>
+// 			<li><a href="/categories/electronics/products">Electronics</a></li>
+// 			<li><a href="/categories/beauty/products">Beauty</a></li>
+// 			<li><a href="/categories/toys-games/products">Toys and Games</a></li>
+// 			<li><a href="/categories/health/products">Health</a></li>
+// 			<li><a href="/categories/furniture/products">Furniture</a></li>
+// 			<li><a href="/categories/clothing/products">Clothing</a></li>
+// 		</ul><button><a href="/products/new">Add a Product</a></button>
+// 	</nav>
+// 	<h1>Products List</h1>
+// 	<ul>
+// 		<li><a href="/products/1">Facial Cleansing Brush</a></li>
+// 	</ul>
+// </body>
+
+// </html>
