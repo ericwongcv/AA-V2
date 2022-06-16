65b6aeee0b23645ae92a10f45a0f501ff95dce64
Eric Wong <contact.eric.wong@gmail.com>
2022-06-15 21:12:43 -0700

    submit json practice

---
+++ practice-for-week-08-serialize-deserialize-json/server.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const { json } = require('stream/consumers');
+
+const server = http.createServer((req, res) => {
+  console.log(`${req.method} ${req.url}`);
+
+  let reqBody = "";
+  req.on("data", (data) => {
+    reqBody += data;
+  });
+
+  req.on("end", () => {
+    // Parse the body of the request as JSON if Content-Type header is
+      // application/json
+
+    const header = req.headers['content-type'];
+
+    if (header === "application/json") {
+      return JSON.parse(reqBody);
+    }
+    // Parse the body of the request as x-www-form-urlencoded if Content-Type
+      // header is x-www-form-urlencoded
+    else if (header === 'x-www-form-urlencoded') {
+      req.body = reqBody
+        .split("&")
+        .map((keyValuePair) => keyValuePair.split("="))
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .reduce((acc, [key, value]) => {
+          acc[key] = value;
+          return acc;
+        }, {});
+
+      // Log the body of the request to the terminal
+      console.log(req.body);
+    }
+
+    const resBody = {
+      "Hello": "World!"
+    };
+
+    // Return the `resBody` object as JSON in the body of the response
+    return JSON.stringify(resBody);
+  });
+});
+
+const port = 5000;
+
+server.listen(port, () => console.log('Server is listening on port', port));
---
+++ practice-for-week-08-testing-api-endpoints/fetch-requests.js
@@ -0,0 +1,46 @@
+/*
+Make fetch requests in the browser for each of the following tasks.
+Paste your code for fetch requests here once you finish each task.
+*/
+
+/* =============================== Phase 1 ================================ */
+/*
+  Make a request with fetch request to GET /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+
+// fetch('/posts');
+
+// fetch('/posts')
+//   .then(res => res.json());
+
+// fetch('/posts')
+//   .then(res => res.json())
+//   .then(resBody => console.log(resBody));
+
+// OR
+// (async function() {
+//   const res = await fetch('/posts');
+//   const body = await res.json();
+//   console.log(body);
+// })();
+
+/* =============================== Phase 2 ================================ */
+/*
+  Make a request with fetch request to POST /posts and print the response
+  components to the console.
+*/
+
+// Your code here
+
+// fetch('/posts', {
+//   method: 'POST',
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify({
+//     message: "New Post!"
+//   })
+// })
