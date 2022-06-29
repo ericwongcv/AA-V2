49dcfb38ba194c5afa58f66783d7a176c7e03e0d
Eric Wong <contact.eric.wong@gmail.com>
2022-06-28 20:46:25 -0700

    update express route handlers with new handlers

--- practice-for-week-10-express-route-handlers/server/app.js
+++ practice-for-week-10-express-route-handlers/server/app.js
@@ -33,15 +33,49 @@ app.use(express.json());
 //   next();
 // });
 
-app.get("/artists", (req, res) => {
-  const artists = getAllArtists();
-  res.send(artists);
+app.get('/artists/:artistId/albums', (req, res) => {
+  const albums = getAlbumsByArtistId(req.params.artistId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(albums);
 });
 
-app.post('/artists', (req, res) => {
-  const newArtist = addArtist(req.body);
-  res.statusCode = 201;
-  res.send(newArtist);
+app.post('/artists/:artistsId/albums', (req, res) => {
+  const album = addAlbumByArtistId(req.params.artistsId, req.body);
+  res.status(201);
+  res.header('Content-Type', 'application/json');
+  res.send(album);
+});
+
+app.get('/artists/:artistId/songs', (req, res) => {
+  const songs = getSongsByArtistId(req.params.artistId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(songs);
+});
+
+app.get('/artists/:artistId', (req, res) => {
+  const artist = getArtistByArtistId(req.params.artistId);
+  res.status(200);
+  res.send(artist);
+});
+
+app.patch('/artists/:artistId', (req, res) => {
+  const artist = editArtistByArtistId(req.params.artistId, req.body);
+  res.status(201);
+  res.send(artist);
+});
+
+app.delete('/artists/:artistId', (req, res) => {
+  deleteArtistByArtistId(req.params.artistId);
+  res.status(200);
+  res.header("Content-Type", "application/json");
+  res.send("Successfully deleted");
+});
+
+app.get('/artists/latest/albums', (req, res) => {
+  const latestArtistAlbums = getAlbumsForLatestArtist();
+  res.send(latestArtistAlbums);
 });
 
 app.get('/artists/latest', (req, res) => {
@@ -49,10 +83,80 @@ app.get('/artists/latest', (req, res) => {
   res.send(latestArtist);
 });
 
-app.get('/artists/latest/albums', (req, res) => {
-  const latestArtistAlbums = getAlbumsForLatestArtist();
-  res.send(latestArtistAlbums);
+app.get("/artists", (req, res) => {
+  const artists = getAllArtists();
+  res.send(artists);
 });
+
+app.post('/artists', (req, res) => {
+  const newArtist = addArtist(req.body);
+  res.status(201);
+  res.send(newArtist);
+});
+
+app.get('/albums/:albumId/songs', (req, res) => {
+  const songs = getSongsByAlbumId(req.params.albumId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(songs);
+});
+
+app.post('/albums/:albumId/songs', (req, res) => {
+  const song = addSongByAlbumId(req.params.albumId, req.body);
+  res.status(201);
+  res.header('Content-Type', 'application/json');
+  res.send(song);
+});
+
+app.get('/albums/:albumId', (req, res) => {
+  const album = getAlbumByAlbumId(req.params.albumId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(album);
+});
+
+app.patch('/albums/:albumId', (req, res) => {
+  const album = editAlbumByAlbumId(req.params.albumId, req.body);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(album);
+});
+
+app.delete('/albums/:albumId', (req, res) => {
+  deleteAlbumByAlbumId(req.params.albumId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send("Successfully deleted");
+});
+
+app.get('/albums', (req, res) => {
+  const albums = getFilteredAlbums(req.query.startsWith);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(albums);
+});
+
+app.get('/songs/:songId', (req, res) => {
+  const song = getSongBySongId(req.params.songId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(song);
+});
+
+app.patch('/songs/:songId', (req, res) => {
+  const song = editSongBySongId(req.params.songId, req.body);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send(song);
+});
+
+app.delete('/songs/:songId', (req, res) => {
+  deleteSongBySongId(req.params.songId);
+  res.status(200);
+  res.header('Content-Type', 'application/json');
+  res.send("Successfully deleted");
+});
+
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
