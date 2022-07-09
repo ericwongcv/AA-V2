56d187140646e6f77a156dd9232ae2c3aeb4e956
Eric Wong <contact.eric.wong@gmail.com>
2022-07-09 00:59:48 -0700

    submit sequelize models short practice

---
+++ practice-for-week-11-sequelize-models-short-practice/server/db/migrations/20220709070133-create-colors.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Colors', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Colors');
+  }
+};
