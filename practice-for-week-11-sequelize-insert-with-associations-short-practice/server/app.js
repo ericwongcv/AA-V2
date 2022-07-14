db05ae4b14f73dde4226952e0d9f0b84f2a63436
Eric Wong <contact.eric.wong@gmail.com>
2022-07-13 21:17:55 -0700

    submit sequelize insert with associations short practice

---
+++ practice-for-week-11-sequelize-insert-with-associations-short-practice/server/app.js
@@ -0,0 +1,95 @@
+// Instantiate Express and the application - DO NOT MODIFY
+const express = require('express');
+const app = express();
+
+// Import environment variables in order to connect to database - DO NOT MODIFY
+require('dotenv').config();
+require('express-async-errors');
+
+// Import the models used in these routes - DO NOT MODIFY
+const { Band, Musician, Instrument } = require('./db/models');
+
+// Express using json - DO NOT MODIFY
+app.use(express.json());
+
+// Import sequelize operators
+const { Op } = require("sequelize");
+
+// STEP 1: Creating from an associated model (One-to-Many)
+app.post('/bands/:bandId/musicians', async (req, res, next) => {
+    // Your code here
+    const band = await Band.findOne({ where: { id: req.params.bandId } });
+    const musician = await band.createMusician({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    });
+
+    const response = {
+        message: 'Created new musician for band ' + band.name,
+        musician: musician
+    };
+
+    res.json(response);
+})
+
+// STEP 2: Connecting two existing records (Many-to-Many)
+app.post('/musicians/:musicianId/instruments', async (req, res, next) => {
+    // Your code here
+    const musician = await Musician.findOne({ where: { id: req.params.musicianId } } );
+    const instruments = await Instrument.findAll({ where: {
+        id: {
+            [Op.or]: req.body.instrumentIds
+        }
+    } });
+    musician.addInstruments(instruments);
+
+    res.json({message: `Associated ${musician.firstName} with instruments ${req.body.instrumentIds}`});
+})
+
+
+// Get the details of one band and associated musicians - DO NOT MODIFY
+app.get('/bands/:bandId', async (req, res, next) => {
+    const payload = await Band.findByPk(req.params.bandId, {
+        include: { model: Musician },
+        order: [[Musician, 'firstName']]
+    });
+    res.json(payload);
+});
+
+// Get the details all bands and associated musicians - DO NOT MODIFY
+app.get('/bands', async (req, res, next) => {
+    const payload = await Band.findAll({
+        include: {model: Musician}, 
+        order: [['name'], [Musician, 'firstName']]
+    });
+    res.json(payload);
+});
+
+// Get the details of one musician and associated instruments - DO NOT MODIFY
+app.get('/musicians/:musicianId', async (req, res, next) => {
+    const payload = await Musician.findByPk(req.params.musicianId, {
+        include: {model: Instrument},
+        order: [[Instrument, 'type']]
+    });
+    res.json(payload);
+});
+
+// Get the details all musicians and associated instruments - DO NOT MODIFY
+app.get('/musicians', async (req, res, next) => {
+    const payload = await Musician.findAll({
+        include: { model: Instrument }, 
+        order: [['firstName'], ['lastName'], [Instrument, 'type']]
+    });
+    res.json(payload);
+});
+
+// Root route - DO NOT MODIFY
+app.get('/', (req, res) => {
+    res.json({
+        message: "API server is running"
+    });
+});
+
+// Set port and listen for incoming requests - DO NOT MODIFY
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
