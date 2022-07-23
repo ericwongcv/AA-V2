59cd46b2bae58ef7cc079149d527c8b16f9853e1
Eric Wong <contact.eric.wong@gmail.com>
2022-07-23 01:35:13 -0700

    add additional practice on navigation

--- react-router-intro/my-app/src/index.js
+++ react-router-intro/my-app/src/index.js
@@ -2,23 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link, NavLink, Switch } from 'react-router-dom';
 import Users from './Users';
 import Profile from './components/Profile';
 
 const Root = () => {
+  const handleClick = () => {
+    console.log('Thanks for clicking!');
+  };
+
   return (
     <BrowserRouter>
       <div>
-        <Route exact path='/'>
-          <App />
-        </Route>
-        <Route exact path='/users'>
-          <Users />
-        </Route>
-        <Route path='/users/:userId'>
-          <Profile />
-        </Route>
+        <NavLink exact to='/' activeClassName='active red' activeStyle={{fontWeight: 'bold'}}>App</NavLink>
+        <NavLink to='/users' activeClassName='active green'>Users</NavLink>
+        <NavLink to='/users/1' activeClassName='active blue'>Andrew's Profile</NavLink>
+
+        {/* Link with onClick prop */}
+        <NavLink exact to='/' onClick={handleClick}>App with click handler</NavLink>
+
+        <Switch>
+          <Route exact path='/'>
+            <App />
+          </Route>
+          <Route exact path='/users'>
+            <Users />
+          </Route>
+          <Route path='/users/:userId'>
+            <Profile />
+          </Route>
+          <Route>
+            <h1>404: Page not found</h1>
+          </Route>
+        </Switch>
+        
       </div>
     </BrowserRouter>
   );
