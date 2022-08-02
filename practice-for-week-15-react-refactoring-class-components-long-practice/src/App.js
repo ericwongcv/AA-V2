970018e166495d6e32f23bbb6f442c5a7c0dfab0
Eric Wong <contact.eric.wong@gmail.com>
2022-08-02 12:21:33 -0700

    submit react refactoring class components long practice

---
+++ practice-for-week-15-react-refactoring-class-components-long-practice/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import Clock, { ClockToggle } from './components/Clock';
+import Folder from './components/Folder';
+import Weather from './components/Weather';
+import AutoComplete from './components/Auto';
+
+const names = [
+  'Abba',
+  'Barbara',
+  'Barney',
+  'Jeff',
+  'Jenny',
+  'Sally',
+  'Sarah',
+  'Xander'
+];
+
+const folders = [
+  { title: 'one', content: 'I am the first' },
+  { title: 'two', content: 'Second folder here' },
+  { title: 'three', content: 'Third folder here' }
+];
+
+export default function App() {
+  const [showClock, setShowClock] = useState(true);
+
+  const toggleClock = () => setShowClock(!showClock);
+
+  return (
+    <div className="widgets">
+      <Folder folders={folders} />
+      <Weather />
+      <ClockToggle toggleClock={toggleClock} />
+      {showClock && <Clock />}
+      <AutoComplete names={names} />
+    </div>
+  );
+};
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showClock: true
+//     };
+//   }
+
+//   toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+//   render() {
+//     return (
+//       <div className="widgets">
+//         <Folder folders={folders} />
+//         <Weather />
+//         <ClockToggle toggleClock={this.toggleClock} />
+//         {this.state.showClock && <Clock />}
+//         <AutoComplete names={names} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
