44f543fd31ce33c8efc841f6856504f049fbce3d
Eric Wong <contact.eric.wong@gmail.com>
2022-08-04 18:05:59 -0700

    submit react redux grocery store long practice checkpoint pt2

--- practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
+++ practice-for-week-15-react-redux-grocery-store-long-practice/src/store/produce.js
@@ -1,6 +1,19 @@
+import produceData from '../mockData/produce.json';
+
+const POPULATE = 'produce/POPULATE';
+
+export const populateProduce = () => {
+    return {
+        type: POPULATE,
+        produce: produceData
+    };
+};
+
 export default function produceReducer(state = {}, action) {
     switch (action.type) {
+        case POPULATE:
+            return {...action.produce};
         default:
             return state;
-    }
-}
+    };
+};
