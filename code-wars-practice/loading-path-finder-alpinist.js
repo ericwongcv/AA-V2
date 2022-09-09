55144d4797b9145bca113023c23a20b6f1449a43
Eric Wong <contact.eric.wong@gmail.com>
2022-09-09 11:07:26 -0700

    submit codewars parth-finder Dijkstra Algo

--- code-wars-practice/loading-path-finder-alpinist.js
+++ code-wars-practice/loading-path-finder-alpinist.js
@@ -5,19 +5,23 @@ function pathFinder(area) {
     const vertex = {};
     vertex[[0, 0]] = 0;
     visited[[0, 0]] = true;
-    
+    const inQ = {};
+
+    inQ[[0, 0]] = true;
     let queue = [[0, 0]];
 
     while (queue.length > 0) {
-        const node = queue[0];
+        const node = queue.pop();
         const [i, j] = [node[0], node[1]];
-        queue = queue.slice(1);
 
         deltas.forEach(delta => {
             const [newI, newJ] = [delta[0] + i, delta[1] + j];
 
             if (newI >= 0 && newI < area.length && newJ >= 0 && newJ < area.length && visited[[newI, newJ]] === undefined) {
-                queue.push([newI, newJ]);
+                if (!inQ[[newI, newJ]]) {
+                    queue.push([newI, newJ]);
+                    inQ[[newI, newJ]] = true;
+                }
 
                 const deltAlt = Math.abs(area[i][j] - area[newI][newJ]);
 
@@ -28,11 +32,10 @@ function pathFinder(area) {
                 }
             }
         })
+        
+        queue.sort((a, b) => vertex[b] - vertex[a]);
 
-        let smallest;
-        queue.forEach(key => {
-            if (smallest === undefined || vertex[key] < smallest) smallest = key
-        });
+        let smallest = queue[queue.length - 1];
 
         visited[smallest] = true;
     }
@@ -40,35 +43,35 @@ function pathFinder(area) {
     return vertex[[area.length - 1, area.length - 1]];
 }
 
-// console.log(pathFinder(
-//     `000
-// 000
-// 000`
-// ));
+console.log(pathFinder(     // 0
+    `000
+000
+000`
+));
 
-// console.log(pathFinder(
-//     `010
-// 101
-// 010`
-// ));
+console.log(pathFinder(     // 4
+    `010
+101
+010`
+));
 
-// console.log(pathFinder(
-//     `700000
-// 077770
-// 077770
-// 077770
-// 077770
-// 000007`
-// ))
+console.log(pathFinder(     //14
+    `700000
+077770
+077770
+077770
+077770
+000007`
+))
 
-// console.log(pathFinder(
-//     `777000
-// 007000
-// 007000
-// 007000
-// 007000
-// 007777`
-// ))
+console.log(pathFinder(     // 0
+    `777000
+007000
+007000
+007000
+007000
+007777`
+))
 
 console.log(pathFinder(     // 18
     `6935268
