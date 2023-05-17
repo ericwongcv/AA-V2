6490b0ed2dc1d08c29605b7feb954e47864e0171
Eric Wong <erics-macbook-air@Erics-MacBook-Air.local>
2023-05-17 11:55:36 -0700

    test

---
+++ test.js
@@ -0,0 +1,28 @@
+// const car = {
+//     color: 'blue',
+//     'type': 'sedan'
+// }
+
+// car["tires"] = 4;
+
+// console.log(car['tires'])
+// console.log(Object.values(car))
+
+// var twoSum = function(nums, target) {
+//     const hash = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         if (hash[nums[i]] !== undefined) return [hash[nums[i]], i];
+//         else hash[target - nums[i]] = i;
+//     }
+//     // return hash;
+// };
+
+// const nums = [2,7,11,15];
+// const target = 9;
+
+// console.log(twoSum(nums, target));
+
+console.log(parseInt("00123"));
+console.log([0,0,3,1].join(""));
+console.log(typeof([1,2,3]))
+console.log("123".split(""))
